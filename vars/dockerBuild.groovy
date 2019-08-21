f9753c96fc94ff559eb15509acccc0e593bfd4cf
daniel.alejandro.hc@gmail.com <daniel.alejandro.hc@gmail.com>
2019-08-21 17:17:48 -0600

    update shared library

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,15 +1,35 @@
 def call(args) {
     def MANDATORY_ARGS = ['credentialsId', 'registry', 'registryOrg', 'imageName']
 
-    //Check for missing (mandatory) parameters
+    // Check for missing (mandatory) parameters
     def given_args = args.keySet();
-    def missing_args = MANDATORY_ARGS - given_args;
-    if (!missing_args.isEmpty()) {
-        error("[CEE Jenkins Shared Library Missing Arguments: ${missing_args}")
+    MANDATORY_ARGS.each {
+        mandatoryArg ->
+            if(args.containsKey(mandatoryArg))
+                error "Error: ${mandatoryArg} is missing. Custom step: 'dockerBuild'"
     }
 
+    // Assign default values
+    // If 'dockerfile' parameter does not exists then It will assign the default dockerfile name: 'Dockerfile'
+    if(!args.containsKey("dockerFile")) {
+        args.dockerFile = "Dockerfile";
+    }
+
+    // If 'path' parameter does not exists then It will assign the default path, which is the current path: '.'
+    if(!args.containsKey("path")) {
+        args.path = ".";
+    }
+
+    if(!args.containsKey("dockerTag")) {
+        args.dockerTag = "latest";
+    }
 
     node {
-        echo "Hello ${args}"
+        echo "Arguments: ${args}";        
+        
+        withCredentials([usernamePassword(credentialsId: args.credentialsId, passwordVariable: 'password', usernameVariable: 'username')]) {
+            sh "docker login -u ${username} -p ${password} https://${registry}";
+            sh "docker build -t ${args.registry}/${args.registryOrg}/${args.imageName}:${args.dockerTag} ${args.path}"
+        }
     }
 }
