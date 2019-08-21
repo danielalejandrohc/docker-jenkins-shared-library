86c9ea7f02112ef8c960a41c7a9a40b1c8573c7c
daniel.alejandro.hc@gmail.com <daniel.alejandro.hc@gmail.com>
2019-08-21 16:19:14 -0600

    Updates

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,15 @@
-def call(map) {
+def call(args) {
+    def MANDATORY_ARGS = ['credentialsId', 'registry', 'registryOrg', 'imageName']
+
+    //Check for missing (mandatory) parameters
+    def given_args = args.keySet();
+    def missing_args = MANDATORY_ARGS - given_args;
+    if (!missing_args.isEmpty()) {
+        error("[CEE Jenkins Shared Library Missing Arguments: ${missing_args}")
+    }
+
+
     node {
-        echo "Hello ${map}"
+        echo "Hello ${args}"
     }
 }
