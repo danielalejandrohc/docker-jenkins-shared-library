653141ed81d85050fda6af63b3ae2670951cf5c6
danielalejandrohc <daniel.alejandro.hc@gmail.com>
2019-09-30 21:41:28 -0600

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -8,7 +8,7 @@
     'dockerTag': (Optional) If not specified the default value will be: 'latest'
 */
 def call(args) {
-    def MANDATORY_ARGS = ['credentialsId', 'registry', 'registryOrg', 'imageName']
+    def MANDATORY_ARGS = ['credentialId', 'registry', 'registryOrg', 'imageName']
 
     // Check for missing (mandatory) parameters
     def given_args = args.keySet();
@@ -29,9 +29,13 @@ def call(args) {
         args.dockerTag = "latest";
     }
 
+    if(!args.containsKey("deleteImages")) {
+        args.deleteImages = false;
+    }
+
     echo "Arguments: ${args}";        
            
-    withCredentials([usernamePassword(credentialsId: args.credentialsId, passwordVariable: 'password', usernameVariable: 'username')]) {
+    withCredentials([usernamePassword(credentialsId: args.credentialId, passwordVariable: 'password', usernameVariable: 'username')]) {
         try {
             sh "docker login -u ${username} -p ${password} https://${args.registry}";
             echo "Docker login perfomed."
@@ -39,5 +43,19 @@ def call(args) {
             error "Error docker login ${e}"
         }
     }
+    sh "docker tag ${args.registryOrg}/${args.imageName}:${args.dockerTag} ${args.registry}/${args.registryOrg}/${args.imageName}:${args.dockerTag}";
     sh "docker push ${args.registry}/${args.registryOrg}/${args.imageName}:${args.dockerTag}";
+    
+    // If deleteImages parameter is passed as true the docker rmi will be performed
+    if(args.deleteImages) {
+        sh "docker rmi ${args.registry}/${args.registryOrg}/${args.imageName}:${args.dockerTag}";
+        // Delete images from dockerhub no tagged with organization nor registry. If found then It will be deleted.
+        output = sh script: "docker images -q ${args.imageName}:${args.dockerTag}", returnStdout: true;
+        echo "Output: ${output}";
+        
+        if(!output.trim().isEmpty()) {
+            sh "docker rmi ${args.imageName}:${args.dockerTag}";
+        }
+    }
+    
 }
